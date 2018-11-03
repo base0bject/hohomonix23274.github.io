042bacedad4d9a783ed8d31043c902899650c922
hohomonix23274 <nikitamanukol23274@gmail.com>
2018-11-03 13:35:50 +0300

    site

---
+++ js/magnific-popup-options.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // MagnificPopup
+  var magnifPopup = function() {
+    $('.image-popup').magnificPopup({
+      type: 'image',
+      removalDelay: 300,
+      mainClass: 'mfp-with-zoom',
+      gallery:{
+        enabled:true
+      },
+      zoom: {
+        enabled: true, 
+
+        duration: 300, 
+        easing: 'ease-in-out', 
+
+        
+        opener: function(openerElement) {
+        
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+  };
+
+  
+  // Call the functions 
+  magnifPopup();
+
+});
